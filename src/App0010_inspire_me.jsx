e6a8926a80e326f33edf906e1c4e11c0933d3202
deepakdevfile <deepakdevfile@gmail.com>
2026-08-10 05:49:17 +0530

    C9: Render tree and Dependency tree

---
+++ src/App0010_inspire_me.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+let quotes = [
+    "Don’t let yesterday take up too much of today.” — Will Rogers",
+    "Ambition is putting a ladder against the sky.",
+    "A joy that's shared is a joy made double.",
+];
+
+function FancyText({ title, text }) {
+    return title
+        ? <h1 className='fancy title'>{text}</h1>
+        : <h3 className='fancy cursive'>{text}</h3>
+}
+
+function Copyright({ year }) {
+    return <p className='small'>©️ {year}</p>;
+}
+
+function InspirationGenerator({ children }) {
+    const [index, setIndex] = React.useState(0);
+    const quote = quotes[index];
+    const next = () => setIndex((index + 1) % quotes.length);
+
+    return (
+        <>
+            <p>Your inspirational quote is:</p>
+            <FancyText text={quote} />
+            <button onClick={next}>Inspire me again</button>
+            {children}
+        </>
+    );
+}
+
+export default function App() {
+    return (
+        <>
+            <FancyText title text="Get Inspired App" />
+            <InspirationGenerator>
+                <Copyright year={2004} />
+            </InspirationGenerator>
+        </>
+    );
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App0009_c3_Fix_a_broken_story_tray';
+import App from './App0010_inspire_me';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
